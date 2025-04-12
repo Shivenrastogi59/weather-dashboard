9ce10ecba3ab2f350043e03a9afc498cf1cfac23
Shivenrastogi59 <rastogishiven7@gmail.com>
2025-04-13 00:59:19 +0530

    debugging

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const weatherRoute = require('./routes/weather');
 dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000; 
 
 app.use(cors());
 app.use('/weather', weatherRoute);
